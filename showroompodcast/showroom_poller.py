c8909717fd6300ab9f531570fe281f799817bac0
Master <roadmasternavi@gmail.com>
2024-12-23 21:25:45 +0900

    Add retry when connect to API

--- showroompodcast/showroom_poller.py
+++ showroompodcast/showroom_poller.py
@@ -23,9 +23,18 @@ class ShowroomPoller:
         try:
             is_on_live = Polling.poll(room_id)
         except HTTPError as error:
-            # Often returns 503, 504 temporary, retry.
-            raise_if(error.response.status_code not in [503, 504])
+            # Often returns 502, 503, 504 temporary, retry.
+            raise_if(error.response.status_code not in [500, 502, 503, 504])
             self.logger.debug(str(error), exc_info=error)
             return
+        except ConnectionError as error:
+            # To avoid process down due to temporary DNS resolution error.
+            list_error_message_retry = [
+                "Failed to establish a new connection: [Errno -2] Name or service not known",
+                "Failed to establish a new connection: [Errno -3] Temporary failure in name resolution",
+            ]
+            condition_retry = any(message in str(error) for message in list_error_message_retry)
+            raise_if(not condition_retry)
+            self.logger.debug(str(error), exc_info=error)
         if is_on_live and lock_archive_task.acquire(blocking=False):
             self.executor.create_process_task(self.showroom_archiver.archive, room_id, lock_archive_task)
